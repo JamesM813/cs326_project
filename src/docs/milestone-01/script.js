33ff2286a3693e05329bffcc3df7112ce938b01d
JamesM813 <jrm803@gmail.com>
2024-04-02 22:00:04 -0400

    Added JS and CSS

--- src/docs/milestone-01/script.js
+++ src/docs/milestone-01/script.js
@@ -1,3 +1,20 @@
-function add(x,y){
-    return x + y
+let wordCount = 0;
+
+function countWordsInSection(sectionId) {
+  let section = document.getElementById(sectionId);
+  let text = section.innerText;
+  let words = text.split(/\s+/).length;
+  wordCount += words;
 }
+
+// Call the countWordsInSection function for each section when the DOM content is loaded
+document.addEventListener("DOMContentLoaded", function () {
+  countWordsInSection("team");
+  countWordsInSection("overview");
+  countWordsInSection("application-parts");
+  countWordsInSection("data-management");
+  countWordsInSection("wire-frames");
+  countWordsInSection("real-world");
+  countWordsInSection("integrative-experience");
+  document.getElementById("footer").innerHTML += wordCount;
+});

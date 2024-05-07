23787fb1a07e2c1ba4fa07a4378b093a87d719f4
JamesM813 <jrm803@gmail.com>
2024-05-07 15:36:38 -0400

    Installed express, set up some more foundation

--- src/server/milestone-03/db.js
+++ src/server/milestone-03/db.js
@@ -1,4 +1,34 @@
 import PouchDB from "pouchdb";
-import questions from "qu"
-const db = new PouchDB("trivia");
 
+/**
+ * Initializes a PouchDB database with specified collections if they do not
+ * exist.
+ *
+ * This function creates a new PouchDB instance with the given database name. It
+ * attempts to retrieve collections for 'words' and 'games'. If these
+ * collections do not exist, it creates them with initial empty arrays.
+ *
+ * @param {string} dbname - The name of the database to initialize.
+ */
+const initdb = async (dbname) => {
+
+    const db = new PouchDB(dbname);
+  
+    try {
+      const questions = await db.get("questions");
+    } catch (e) {
+      db.put({ _id: "questions", questions: [] });
+    }
+  
+    db.close();
+  };
+
+const Database = (dbname) => {
+    initdb(dbname);
+
+    const getDB = () => new PouchDB(dbname);
+    const obj = {
+        //we cna put db functions in here
+    }
+    return obj
+}
--- src/server/milestone-03/server.js
+++ src/server/milestone-03/server.js
@@ -1,5 +1,11 @@
 import express from "express";
 import * as db from "./db.js";
+import * as http from "http";
+import * as url from "url";
 
 const app = express();
 const port = 3260;
+
+http.createServer(basicServer).listen(port, () => {
+    console.log(`Server started on port ${port}`);
+  });

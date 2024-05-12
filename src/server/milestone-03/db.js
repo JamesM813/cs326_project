eb46309f16a4d4201d9fcc2e09aed8ce87c63108
JamesM813 <jrm803@gmail.com>
2024-05-12 13:32:09 -0400

    Attempted, still some issues

--- src/server/milestone-03/db.js
+++ src/server/milestone-03/db.js
@@ -22,6 +22,7 @@ const Database = async (dbname) => {
     await initdb(dbname);
 
     const getDB = () => new PouchDB(dbname);
+    
     const obj = {
         
         /**
@@ -59,7 +60,7 @@ const Database = async (dbname) => {
                 const sorted = data.sort((a,b) => b.score - a.score)
                 const top10 = sorted.slice(0,10)
                 await db.close();
-                return { status: "success", data: top10Scores };
+                return { status: "success", data: top10 };
             } catch(err){
                 return {
                     status: "Error",
